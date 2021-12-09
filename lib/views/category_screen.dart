80aae94c0834d0eced6197dcec94c587e3752bf4
Jamal Hassouni <ja.hassouni@gmail.com>
2021-12-09 16:47:58 +0100

    Start Working on Categories

--- lib/views/category_screen.dart
+++ lib/views/category_screen.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/widgets/category_item.dart';
 
 class CategoryScreen extends StatelessWidget {
   const CategoryScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(''),
-      ),
-      body: Container(),
-    );
+    return GridView(
+        children: dummyCategories
+            .map((catData) =>
+                CategoryItem(title: catData.title, color: catData.color))
+            .toList(),
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 200,
+            mainAxisSpacing: 200));
   }
 }
