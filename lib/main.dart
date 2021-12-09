1c7318963e0e56fc3404d920a5efa20d55e14fb5
Jamal Hassouni <ja.hassouni@gmail.com>
2021-12-09 17:18:42 +0100

    Adding Styling & Theming

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/views/category_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -10,30 +11,18 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'DeliMeals',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('DeliMeals'),
-      ),
-      body: const Center(
-        child: Text('Navigation Time!'),
-      ),
+          canvasColor: const Color.fromRGBO(255, 254, 229, 1),
+          colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.pink)
+              .copyWith(secondary: Colors.amber),
+          fontFamily: "Raleway",
+          textTheme: ThemeData.light().textTheme.copyWith(
+              bodyText1: const TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              bodyText2: const TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              headline6: const TextStyle(
+                  fontSize: 20,
+                  fontFamily: "RobotoCondensed",
+                  fontWeight: FontWeight.bold))),
+      home: const CategoryScreen(),
     );
   }
 }
--- lib/views/category_screen.dart
+++ lib/views/category_screen.dart
@@ -7,15 +7,22 @@ class CategoryScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-        children: dummyCategories
-            .map((catData) =>
-                CategoryItem(title: catData.title, color: catData.color))
-            .toList(),
-        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('DeliMeals'),
+      ),
+      body: GridView(
+          padding: const EdgeInsets.all(25),
+          children: dummyCategories
+              .map((catData) =>
+                  CategoryItem(title: catData.title, color: catData.color))
+              .toList(),
+          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
             maxCrossAxisExtent: 200,
             childAspectRatio: 3 / 2,
-            crossAxisSpacing: 200,
-            mainAxisSpacing: 200));
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20,
+          )),
+    );
   }
 }
